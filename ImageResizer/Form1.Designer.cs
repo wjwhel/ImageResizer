3ba4fa38d6dd5567fb6cb00b7fce2a1c3c805dd1
Warren <warrenjw17@gmail.com>
2021-06-02 22:05:20 +0800

    Add project files.

---
+++ ImageResizer/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace ImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(12, 131);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(104, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Reisze";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(12, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(356, 25);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ImageResizer.Properties.Resources.folder;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Infolder...";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::ImageResizer.Properties.Resources.folder;
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Outfolder...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(12, 82);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 15);
+            this.status.TabIndex = 5;
+            this.status.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(384, 186);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSubmit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RMA Image Resizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label status;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ImageResizer/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//
+using System.IO;
+using System.Collections;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ImageResizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Value = 0;
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker1.WorkerReportsProgress = true;
+
+            backgroundWorker1.RunWorkerCompleted +=
+                new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+            backgroundWorker1.ProgressChanged += 
+                new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            btnSubmit.Enabled = false;
+            status.Visible = true;
+
+            string[] parentDir = Directory.GetDirectories(Properties.Settings.Default.InputFolder);
+
+            string[] subDir = Directory.GetDirectories(Properties.Settings.Default.InputFolder, "*", SearchOption.AllDirectories);
+
+            ArrayList allDir = new ArrayList(subDir);
+
+            for (int i = 0; i < parentDir.Length; i++)
+            {
+                if (allDir.Contains(parentDir[i]))
+                {
+                    allDir.Remove(parentDir[i]);
+                }
+            }
+
+            if (backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync(allDir);
+            }
+        }
+        public static Bitmap resizeImage(Image image)
+        {
+            var destRect = new Rectangle(0, 0, 1024, 768);
+            var destImage = new Bitmap(1024, 768);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return destImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            Properties.Settings.Default.InputFolder = folderBrowserDialog1.SelectedPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            Properties.Settings.Default.OutputFolder = folderBrowserDialog1.SelectedPath;
+            Properties.Settings.Default.Save();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            ArrayList allDir = (ArrayList)e.Argument;
+            int count = 0;
+            foreach (string dir in allDir)
+            {
+                count++;
+                int percent = (count * 100) / allDir.Count;
+                string RMA_SN = dir.Replace(Properties.Settings.Default.InputFolder + "\\", "");
+
+                string[] subs = RMA_SN.Split("\\");
+
+                string RMA = subs[0];
+
+                string SN = subs[1];
+
+                string[] outfolder = Directory.GetDirectories(Properties.Settings.Default.OutputFolder, RMA + "*");
+
+                DirectoryInfo di = Directory.CreateDirectory(outfolder[0] + "\\Inspection Photos\\" + SN);
+
+                string[] files = Directory.GetFiles(dir);
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Image newImage = Image.FromFile(files[i]);
+
+                    Image resizedImage = resizeImage(newImage);
+
+                    resizedImage.Save(di.FullName + "\\" + RMA + "_" + SN + "_" + Path.GetFileName(files[i]), ImageFormat.Jpeg);
+                }
+                worker.ReportProgress(percent, RMA + "/" + SN);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            status.Text = "Resizing... " + e.UserState.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else
+            {
+                status.Text = "Complete.";
+                progressBar1.Value = 100;
+                btnSubmit.Enabled = true;
+            }
+        }
+    }
+}
